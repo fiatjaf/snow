e752dbd5b25463e51b02f618902b1ae31dc087a0
fiatjaf <fiatjaf@gmail.com>
2023-03-24 16:06:05 -0300

    nip19 decoder.

---
+++ src/main/scala/NIP19.scala
@@ -0,0 +1,171 @@
+package snow
+
+import scala.util.{Try, Success, Failure}
+import scodec.bits.*
+import scoin.{PrivateKey, XOnlyPublicKey, Bech32, ByteVector32}
+
+case class EventPointer(
+    id: String,
+    relays: List[String],
+    author: Option[XOnlyPublicKey]
+)
+
+case class ProfilePointer(
+    pubkey: XOnlyPublicKey,
+    relays: List[String]
+)
+
+case class Address(
+    d: String,
+    kind: Int,
+    author: XOnlyPublicKey,
+    relays: List[String]
+)
+
+enum TLVType:
+  case Special
+  case Relays
+  case Author
+  case Kind
+  case Unknown
+
+object TLVType {
+  def fromByte(b: ByteVector): TLVType = fromByte(b(0))
+
+  def fromByte(b: Byte): TLVType = b match {
+    case 0 => TLVType.Special
+    case 1 => TLVType.Relays
+    case 2 => TLVType.Author
+    case 3 => TLVType.Kind
+    case _ => TLVType.Unknown
+  }
+}
+
+object NIP19 {
+  def decode(bech32text: String): Either[
+    Throwable,
+    PrivateKey | XOnlyPublicKey | String | EventPointer | ProfilePointer |
+      Address,
+  ] =
+    Try(Bech32.decode(bech32text)) match {
+      case Failure(err) => Left(err)
+      case Success((_, _, enc)) if enc == Bech32.Bech32mEncoding =>
+        Left(Error("encoding is in bech32, not bech32m"))
+      case Success((prefix, data5, _)) =>
+        val data = Bech32.five2eight(data5)
+        prefix match {
+          case "npub"     => parseNpub(data)
+          case "nsec"     => parseNsec(data)
+          case "note"     => parseNote(data)
+          case "nprofile" => parseNprofile(data)
+          case "nevent"   => parseNevent(data)
+          case "naddr"    => parseNaddr(data)
+          case _          => Left(Error(s"unsupported prefix '$prefix'"))
+        }
+    }
+
+  def parseNpub(data: Array[Byte]): Either[Throwable, XOnlyPublicKey] =
+    if data.size == 32 then
+      Right(XOnlyPublicKey(ByteVector32(ByteVector(data))))
+    else Left(Error("npub must contain 32 bytes"))
+
+  def parseNsec(data: Array[Byte]): Either[Throwable, PrivateKey] =
+    if data.size == 32 then Right(PrivateKey(ByteVector32(ByteVector(data))))
+    else Left(Error("nsec must contain 32 bytes"))
+
+  def parseNote(data: Array[Byte]): Either[Throwable, String] =
+    if data.size == 32 then Right(ByteVector(data).toHex)
+    else Left(Error("note must contain 32 bytes"))
+
+  def parseNprofile(data: Array[Byte]): Either[Throwable, ProfilePointer] = {
+    var pubkey: XOnlyPublicKey = null
+    var relays: List[String] = List.empty
+    parseTLV(data).foreach {
+      case TLVRecord(TLVType.Special, v) =>
+        if (v.size == 32) {
+          pubkey = XOnlyPublicKey(ByteVector32(v))
+        }
+      case TLVRecord(TLVType.Relays, v) =>
+        relays = v.decodeUtf8Lenient :: relays
+      case _ =>
+    }
+    if pubkey != null then Right(ProfilePointer(pubkey, relays))
+    else Left(Error("nprofile pubkey record missing or invalid"))
+  }
+
+  def parseNevent(data: Array[Byte]): Either[Throwable, EventPointer] = {
+    var id: String = null
+    var relays: List[String] = List.empty
+    var author: Option[XOnlyPublicKey] = None
+    parseTLV(data).foreach {
+      case TLVRecord(TLVType.Special, v) =>
+        id = v.toHex
+      case TLVRecord(TLVType.Relays, v) =>
+        relays = v.decodeUtf8Lenient :: relays
+      case TLVRecord(TLVType.Author, v) =>
+        if (v.size == 32) {
+          author = Some(XOnlyPublicKey(ByteVector32(v)))
+        }
+      case _ =>
+    }
+    if id != null then Right(EventPointer(id, relays, author))
+    else Left(Error("nevent id record missing or invalid"))
+  }
+
+  def parseNaddr(data: Array[Byte]): Either[Throwable, Address] =
+    var d: String = null
+    var relays: List[String] = List.empty
+    var author: XOnlyPublicKey = null
+    var kind: Int = -1
+    parseTLV(data).foreach {
+      case TLVRecord(TLVType.Special, v) =>
+        d = v.decodeUtf8Lenient
+      case TLVRecord(TLVType.Relays, v) =>
+        relays = v.decodeUtf8Lenient :: relays
+      case TLVRecord(TLVType.Author, v) =>
+        if (v.size == 32) {
+          author = XOnlyPublicKey(ByteVector32(v))
+        }
+      case TLVRecord(TLVType.Kind, v) =>
+        kind = v.toInt(signed = false, ordering = ByteOrdering.BigEndian)
+      case _ =>
+    }
+    if d == null then Left(Error("naddr d record missing or invalid"))
+    else if author == null then
+      Left(Error("naddr author record missing or invalid"))
+    else if kind == -1 then Left(Error("naddr kind record missing or invalid"))
+    else Right(Address(d, kind, author, relays))
+
+  def parseTLV(data: Array[Byte]): List[TLVRecord] =
+    parseTLVRecords(List.empty, ByteVector(data))
+
+  def parseTLVRecords(
+      records: List[TLVRecord],
+      bytes: ByteVector
+  ): List[TLVRecord] = {
+    if (bytes.size == 0) records
+    else
+      parseTLVRecord(bytes)
+        .map((rest, record) => parseTLVRecords(record :: records, rest))
+        .getOrElse(List.empty)
+  }
+
+  def parseTLVRecord(
+      bytes: ByteVector
+  ): Either[String, (ByteVector, TLVRecord)] =
+    bytes.consume(1)(b => Right(TLVType.fromByte(b))).flatMap { (rest, t) =>
+      rest
+        .consume(1)(b =>
+          Right(
+            b.toInt(signed = false, ordering = ByteOrdering.BigEndian)
+          )
+        )
+        .flatMap { (rest, l) =>
+          rest.consume(l)(b => Right(b)).map { (rest, v) =>
+            (rest, TLVRecord(t, v))
+          }
+        }
+    }
+
+  case class TLVRecord(t: TLVType, value: ByteVector)
+}
---
+++ src/test/scala/NIP19Test.scala
@@ -0,0 +1,124 @@
+package snow
+
+import utest._
+import io.circe.syntax._
+import io.circe.parser.{parse, decode}
+import scodec.bits.ByteVector
+import scoin.PrivateKey
+import scoin.XOnlyPublicKey
+
+object NIP19Test extends TestSuite {
+  val tests = Tests {
+    test("decode npub") {
+      val value = NIP19
+        .decode(
+          "npub148ut8u4vr8xqd4gefhg6eyc5636p5zthw3zfse2njfkezegczers59ty0w"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: XOnlyPublicKey => }
+
+      value match {
+        case pk: XOnlyPublicKey =>
+          pk.value.toHex ==> "a9f8b3f2ac19cc06d5194dd1ac9314d4741a09777444986553926d9165181647"
+        case _ =>
+      }
+    }
+
+    test("decode nsec") {
+      val value = NIP19
+        .decode(
+          "nsec1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqsmhltgl"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: PrivateKey => }
+
+      value match {
+        case sk: PrivateKey =>
+          sk.value.toHex ==> "0000000000000000000000000000000000000000000000000000000000000001"
+        case _ =>
+      }
+    }
+
+    test("decode note") {
+      val value = NIP19
+        .decode(
+          "note1yw5agdtgrkwytpy2ch9pahfewhf9dyr2zkl56gjqrf4hnz2wc3lqj8w45q"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: String => }
+
+      value match {
+        case id: String =>
+          id ==> "23a9d435681d9c45848ac5ca1edd3975d256906a15bf4d22401a6b79894ec47e"
+        case _ =>
+      }
+    }
+
+    test("decode nevent") {
+      val value = NIP19
+        .decode(
+          "nevent1qqsz82w5x45pm8z9sj9vtjs7m5uht5jkjp4pt06dyfqp56me398vglspp3mhxue69uhhstnrdakj7q3q9klqjtr2mgfk0m9h8g80z8xjcv07kv340qjvvjsclrdgt93pf4cqur7gsc"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: EventPointer => }
+
+      value match {
+        case evp: EventPointer =>
+          evp.id ==> "23a9d435681d9c45848ac5ca1edd3975d256906a15bf4d22401a6b79894ec47e"
+          evp.relays ==> List("wss://x.com/")
+          evp.author.map(_.value.toHex) ==> Some(
+            "2dbe092c6ada1367ecb73a0ef11cd2c31feb32357824c64a18f8da8596214d70"
+          )
+        case _ =>
+      }
+    }
+
+    test("decode nprofile") {
+      val value = NIP19
+        .decode(
+          "nprofile1qqsw96tn6z4zpgs24enrec7zak9mzcdekt0edf08vrfenln8t4m5v8sppdmhxue69uhhjtnrdakszrrhwden5te00qhxxmmd9um2rgu3"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: ProfilePointer => }
+
+      value match {
+        case pp: ProfilePointer =>
+          pp.pubkey.value.toHex ==> "e2e973d0aa20a20aae663ce3c2ed8bb161b9b2df96a5e760d399fe675d77461e"
+          pp.relays.size ==> 2
+          pp.relays.contains("wss://x.com/")
+          pp.relays.contains("wss://y.com/")
+        case _ =>
+      }
+    }
+
+    test("decode naddr") {
+      val value = NIP19
+        .decode(
+          "naddr1qqrxyctwv9hxzq3qut5h8592yz3q4tnx8n3u9mvtk9smnvklj6j7wcxnn8lxwhthgc0qxpqqqzgauhfurwa"
+        )
+        .toTry
+        .get
+
+      assertMatch(value) { case _: Address => }
+
+      value match {
+        case addr: Address =>
+          addr.author.value.toHex ==> "e2e973d0aa20a20aae663ce3c2ed8bb161b9b2df96a5e760d399fe675d77461e"
+          addr.relays.size ==> 0
+          addr.kind ==> 37342
+          addr.d ==> "banana"
+        case _ =>
+      }
+    }
+  }
+}
